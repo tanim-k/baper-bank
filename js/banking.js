0c2028a994301ed2462d157475272cbb4445c628
tanim-k <tanimkhan877@gmail.com>
2022-02-12 08:31:02 +0600

    bank transaction deposit and balance add and rechecked and problem solved

--- js/banking.js
+++ js/banking.js
@@ -1,37 +1,36 @@
-/// handle deposit button event 1 ////
+/// handle deposit button by  event handeler: 1 /
     document.getElementById('deposit-button').addEventListener('click', function () {
-/// get the amount deposited ::: deposit portion// 
+/// get the amount deposited : deposit portion /
     const depositInput = document.getElementById('deposit-input');
     const newDepositAmountText = depositInput.value;
-    const newDepositAmount = parseFloat(newDepositAmountText);
-/* or this way >   
-/// update deposit total */
+    const newDepositAmount = parseFloat(newDepositAmountText);  
+/// update deposit total /
     document.getElementById('deposit-total');
     const previousDepositText = document.getElementById('deposit-total').innerText;
     const previousDepositAmount = parseFloat(previousDepositText);
     const newDepositTotal = previousDepositAmount + newDepositAmount;
     document.getElementById('deposit-total').innerText = newDepositTotal;
-/// update account balance after deposit
+/// update account balance after deposit /
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
     const newBalanceTotal = previousBalanceTotal + newDepositAmount;
     balanceTotal.innerText = newBalanceTotal;
-/// clear the deposit input field 
+/// clear the deposit input field /
     document.getElementById('deposit-input').value = '';
 })
-/// handle withdraw event handler 
+/// handle withdraw button by event Handler : 2////
     document.getElementById('withdraw-button').addEventListener('click', function () {
     const withdrawInput = document.getElementById('withdraw-input');
     const withdrawAmountText = withdrawInput.value;
     const newWithdrawAmount = parseFloat(withdrawAmountText);
-/// set withdraw total 
+/// set withdraw total /
     const withdarwTotlal = document.getElementById('withdraw-total');
     const previousWithdrawText = withdarwTotlal.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawText);
     const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
     withdarwTotlal.innerText = newWithdrawTotal;
-/// set balance total 
+/// update account balance after withdraw some money /
     const balanceTotal = document.getElementById('balance-total');
     const previousBalanceText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceText);
